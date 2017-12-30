b282f310aeaef7f9f15eab95ca4c19f3e3c2f950
TGots7 <tylergotto17@gmail.com>
2017-12-30 17:08:44 -0500

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'application#index'
   get '/' => 'application#index'
   get '/about' => 'application#about'
   get '/contact' => 'application#contact'
